0a34060dbb761b1fbd791150b2c53b504fd7034e
Aneledyubhele <anele.dyubhele@capaciti.org.za>
2024-03-12 15:33:40 +0200

    changes

---
+++ src.js
@@ -0,0 +1,14 @@
+
+  // JavaScript for toggling the menu
+  window.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menu-toggle');
+    const menu = document.querySelector('.menu');
+    
+    menuToggle.addEventListener('change', function() {
+      if (this.checked) {
+        menu.style.display = 'block';
+      } else {
+        menu.style.display = 'none';
+      }
+    });
+  });
